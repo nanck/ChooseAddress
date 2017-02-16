ac40dd01eeceee25b3fc4d1a99631d6f8f94337e
nanck <tjg356@163.com>
2017-02-16 21:21:08 +0800

    代码中加入版权信息

--- addresschoose/src/main/java/com/nanck/addresschoose/ChooserActivity.java
+++ addresschoose/src/main/java/com/nanck/addresschoose/ChooserActivity.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2017 nanck
+ *
+ * 1999 Free Software Foundation, Inc. 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA Everyone is > > permitted to copy and distribute verbatim copies of this license document,
+ * but changing it is not allowed.
+ * [This is the first released version of the Lesser GPL.
+ * It also counts as the successor of the GNU Library Public License, > > version 2,
+ * hence the version number 2.1.]
+ */
 package com.nanck.addresschoose;
 
 import android.app.Activity;
